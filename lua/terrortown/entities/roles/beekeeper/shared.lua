627bfc5eef688bb793692d94e7faa9e1b19c34d9
Monica Moniot <mamoniot@proton.me>
2026-03-29 13:43:41 -0400

    added last stand mechanic

--- lua/terrortown/entities/roles/beekeeper/shared.lua
+++ lua/terrortown/entities/roles/beekeeper/shared.lua
@@ -5,6 +5,7 @@ end
 
 local flags = {FCVAR_NOTIFY, FCVAR_ARCHIVE, FCVAR_REPLICATED}
 CreateConVar("ttt2_beekeeper_damage_mult", 0.0, flags)
+CreateConVar("ttt2_beekeeper_damage_mult_last_stand", 0.5, flags)
 
 function ROLE:PreInitialize()
   self.color = Color(231, 197, 0)
@@ -56,16 +57,73 @@ if SERVER then
     ply:StripWeapon("weapon_ttt_beegun")
   end
 
-  local function BeekeeperDealDamage(ply, inflictor, killer, amount, dmginfo)
-    -- Allow beekeepers to use explosive weapons.
-    if dmginfo:IsDamageType(DMG_BLAST) then return end
+  local cur_dmg_mult = {}
+  local base_dmg_mult = 0.0
+  local last_stand_dmg_mult = 0.0
 
+  hook.Add("TTTBeginRound", "BeekeeperSelected", function()
+    cur_dmg_mult = {}
+    base_dmg_mult = GetConVar("ttt2_beekeeper_damage_mult"):GetFloat() or 1.0
+    last_stand_dmg_mult = GetConVar("ttt2_beekeeper_damage_mult_last_stand"):GetFloat() or 1.0
+  end)
+
+  hook.Add("PlayerTakeDamage", "BeekeeperDealDamage", function(ply, inflictor, killer, amount, dmginfo)
     local attacker = dmginfo:GetAttacker()
     if not IsValid(attacker) or not attacker:IsPlayer() or attacker:GetSubRole() ~= ROLE_BEEKEEPER then return end
 
-    dmginfo:ScaleDamage(GetConVar("ttt2_beekeeper_damage_mult"):GetFloat() or 1.0)
+    -- Allow beekeepers to use explosive weapons.
+    if dmginfo:IsDamageType(DMG_BLAST) then return end
+
+    dmginfo:ScaleDamage(cur_dmg_mult[attacker:UserID()] or base_dmg_mult)
+  end)
+
+  local function CheckLastStand(dead_ply)
+    local all_players = player.GetAll()
+    for _, beekeeper in ipairs(all_players) do
+      if beekeeper:Alive() and not beekeeper:IsSpec() and beekeeper:GetSubRole() == ROLE_BEEKEEPER then
+        local beekeeper_id = beekeeper:UserID()
+        if not cur_dmg_mult[beekeeper_id] then
+          local beekeeper_team = beekeeper:GetTeam()
+          if not dead_ply or dead_ply:GetTeam() == beekeeper_team then
+            local is_last_standing = true
+            for _, ply in ipairs(all_players) do
+              if ply:Alive() and not ply:IsSpec() and ply:GetTeam() == beekeeper_team then
+                is_last_standing = false
+                break
+              end
+            end
+            if is_last_standing then
+              cur_dmg_mult[beekeeper_id] = last_stand_dmg_mult
+
+              local body
+              if base_dmg_mult < last_stand_dmg_mult then
+                body = "ttt2_beekeeper_last_stand_body_inc"
+              else
+                body = "ttt2_beekeeper_last_stand_body_dec"
+              end
+              EPOP:AddMessage(
+                beekeeper,
+                {
+                  text = LANG.TryTranslation("ttt2_beekeeper_last_stand_title")
+                },
+                LANG.GetParamTranslation(body, {multi = last_stand_dmg_mult}),
+                6
+              )
+            end
+          end
+        end
+      end
+    end
   end
-  hook.Add("PlayerTakeDamage", "BeekeeperDealDamage", BeekeeperDealDamage)
+
+  hook.Add("PlayerDisconnected", "BeekeeperLastStandDisconnect", CheckLastStand)
+  hook.Add("TTT2PostPlayerDeath", "BeekeeperLastStandDeath", CheckLastStand)
+  hook.Add("PlayerSpawn", "BeekeeperLastStandSpawn", function(ply)
+    if not IsValid(ply) or not ply:Alive() or ply:IsSpec() then return end
+    if ply:GetSubRole() == ROLE_BEEKEEPER then
+      CheckLastStand(nil)
+    end
+  end)
 end
 
 if CLIENT then
@@ -75,6 +133,14 @@ if CLIENT then
     form:MakeSlider({
       serverConvar = "ttt2_beekeeper_damage_mult",
       label = "label_ttt2_beekeeper_damage_mult",
+      min = 0.0,
+      max = 1.0,
+      decimal = 2,
+    })
+
+    form:MakeSlider({
+      serverConvar = "ttt2_beekeeper_damage_mult_last_stand",
+      label = "label_ttt2_beekeeper_damage_mult_last_stand",
       min = 0.0,
       max = 1.0,
       decimal = 2,
